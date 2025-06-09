f997e40cdeeda1afb5c78df61c05e3d77436cc9a
xinulsw <xinulsw@gmail.com>
2025-06-09 22:17:01 +0200

    dodanie js do wyświetlania burgera w bulmie

--- quiz/app.py
+++ quiz/app.py
@@ -44,6 +44,7 @@ def index():
     # return 'Cześć, tu Python i Flask!'
     return render_template('index.html')
 
+
 with app.app_context():
     if __name__ == "__main__":
         app.run(debug=True)
